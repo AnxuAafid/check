0867587682c193bf94481cab2486e5b7dee5bc9a
Anxu Aafid <anzarhussainlone@gmail.com>
2025-11-18 10:10:11 +0530

    first check

---
+++ a.py
@@ -0,0 +1,11 @@
+class a:
+    def x(self):
+        print("hy")
+class b:
+    def y(self):
+        print("Hello")
+class c(a,b):
+    def gem(self):
+        print("hellooooooo")
+j = c()
+j.gem()
